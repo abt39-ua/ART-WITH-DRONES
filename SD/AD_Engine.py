0061b5634c4f5e1d131124ba67c1311a3a4cc473
abt39-ua <71825176+abt39-ua@users.noreply.github.com>
2023-09-19 12:23:53 +0200

    Add files via upload

--- SD/AD_Engine.py
+++ SD/AD_Engine.py
@@ -3,6 +3,27 @@ import threading
 
 PORT = 8080
 SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+FIN = "FIN"
+MAX_CONEXIONES = 2
+
+
+def handle_client(conn, addr):
+    print(f"[NUEVA CONEXION] {addr} connected.")
+
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+        msg = conn.recv(msg_length).decode(FORMAT)
+        if msg == FIN:
+            connected = False
+        print(f" He recibido del cliente [{addr}] el mensaje: {msg}")
+        conn.send(f"HOLA CLIENTE: He recibido tu mensaje: {msg} ".encode(FORMAT))
+    print("ADIOS. TE ESPERO EN OTRA OCASION")
+    conn.close()
 
 
 def start():
@@ -18,9 +39,11 @@ def start():
             thread.start()
             print(f"[CONEXIONES ACTIVAS] {CONEX_ACTIVAS}")
             print("CONEXIONES RESTANTES PARA CERRAR EL SERVICIO", MAX_CONEXIONES-CONEX_ACTIVAS)
-
-
-
+        else:
+            print("OOppsss... DEMASIADAS CONEXIONES. ESPERANDO A QUE ALGUIEN SE VAYA")
+            conn.send("OOppsss... DEMASIADAS CONEXIONES. Tendrás que esperar a que alguien se vaya".encode(FORMAT))
+            conn.close()
+            CONEX_ACTUALES = threading.active_count()-1
 
 
 
