f293581dc62e71fbe7be86c20d82f46039c743ca
abt39-ua <71825176+abt39-ua@users.noreply.github.com>
2023-09-22 09:44:15 +0200

    prac1

--- SD/AD_Engine.py
+++ SD/AD_Engine.py
@@ -1,13 +1,14 @@
-import shocket
+import socket 
 import threading
 
-PORT = 8080
+
+HEADER = 64
+PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 FIN = "FIN"
 MAX_CONEXIONES = 2
-
 
 def handle_client(conn, addr):
     print(f"[NUEVA CONEXION] {addr} connected.")
@@ -17,25 +18,26 @@ def handle_client(conn, addr):
         msg_length = conn.recv(HEADER).decode(FORMAT)
         if msg_length:
             msg_length = int(msg_length)
-        msg = conn.recv(msg_length).decode(FORMAT)
-        if msg == FIN:
-            connected = False
-        print(f" He recibido del cliente [{addr}] el mensaje: {msg}")
-        conn.send(f"HOLA CLIENTE: He recibido tu mensaje: {msg} ".encode(FORMAT))
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == FIN:
+                connected = False
+            print(f" He recibido del cliente [{addr}] el mensaje: {msg}")
+            conn.send(f"HOLA CLIENTE: He recibido tu mensaje: {msg} ".encode(FORMAT))
     print("ADIOS. TE ESPERO EN OTRA OCASION")
     conn.close()
-
+    
+        
 
 def start():
     server.listen()
-    print(f"[LISTENING] Engine a la escucha en {SERVER}")
-        CONEX_ACTIVAS = threading.active_count()-1
+    print(f"[LISTENING] Servidor a la escucha en {SERVER}")
+    CONEX_ACTIVAS = threading.active_count()-1
     print(CONEX_ACTIVAS)
     while True:
-                conn, addr = server.accept()
+        conn, addr = server.accept()
         CONEX_ACTIVAS = threading.active_count()
         if (CONEX_ACTIVAS <= MAX_CONEXIONES): 
-                        thread = threading.Thread(target=handle_client, args=(conn, addr))
+            thread = threading.Thread(target=handle_client, args=(conn, addr))
             thread.start()
             print(f"[CONEXIONES ACTIVAS] {CONEX_ACTIVAS}")
             print("CONEXIONES RESTANTES PARA CERRAR EL SERVICIO", MAX_CONEXIONES-CONEX_ACTIVAS)
@@ -44,10 +46,10 @@ def start():
             conn.send("OOppsss... DEMASIADAS CONEXIONES. Tendrás que esperar a que alguien se vaya".encode(FORMAT))
             conn.close()
             CONEX_ACTUALES = threading.active_count()-1
-
-
+        
 
 ######################### MAIN ##########################
+
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
