00894c1cf04642afdc733647b864702184e95da8
abt39-ua <71825176+abt39-ua@users.noreply.github.com>
2023-10-02 10:02:57 +0200

    Update AD_Engine.py
    engine clses

--- SD/AD_Engine.py
+++ SD/AD_Engine.py
@@ -13,7 +13,7 @@ PORT = 5050
 FORMAT = 'utf-8'
 FIN = "FIN"
 
-def send(msg):
+def send(msg, client):
     message = msg.encode(FORMAT)
     msg_length = len(message)
     send_length = str(msg_length).encode(FORMAT)
@@ -21,33 +21,43 @@ def send(msg):
     client.send(send_length)
     client.send(message)
     
-########## MAIN ##########8
 
-if  (len(sys.argv) == 4):
-    SERVER = sys.argv[1]
-    PORT = int(sys.argv[2])
-    ADDR = (SERVER, PORT)
-    
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(ADDR)
-    print (f"Establecida conexión en [{ADDR}]")
+def getWeather():
+    if  (len(sys.argv) == 4):
+        SERVER = sys.argv[1]
+        PORT = int(sys.argv[2])
+        ADDR = (SERVER, PORT)
+        
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect(ADDR)
+        print (f"Establecida conexión en [{ADDR}]")
 
-    msg=sys.argv[3]
-    while msg != FIN :
-        print("Envio al servidor: ", msg)
-        send(msg)
-        print("Recibo del Servidor: ", client.recv(2048).decode(FORMAT))
-        msg=input()
-    print("Envio al servidor: ", FIN)
-    send(FIN)
-    client.close()
-else:
-    print ("Oops!. Parece que algo falló. Necesito estos argumentos: <ServerIP> <Puerto> <Ciudad>")
+        msg=sys.argv[3]
+        while msg != FIN :
+            print("Envio al servidor: ", msg)
+            send(msg, client)
+            print("Recibo del Servidor: ", client.recv(2048).decode(FORMAT))
+            msg=input()
+        print("Envio al servidor: ", FIN)
+        send(FIN)
+        client.close()
+    else:
+        print ("Oops!. Parece que algo falló. Necesito estos argumentos: <ServerIP> <Puerto> <Ciudad>")
 
-# Crear una matriz 2D de 20x20 posiciones para representar el espacio aéreo
-espacio_aereo = [[0 for _ in range(20)] for _ in range(20)]
+    # Crear una matriz 2D de 20x20 posiciones para representar el espacio aéreo
+    espacio_aereo = [[0 for _ in range(20)] for _ in range(20)]
 
-# Imprimir la matriz para visualizar el espacio aéreo
-for fila in espacio_aereo:
-    print(fila)
+    # Imprimir la matriz para visualizar el espacio aéreo
+    for fila in espacio_aereo:
+        print(fila)
 
+def getFigura():
+    print("hola")
+
+
+########## MAIN ##########
+
+print("Consiguiendo información del tiempo...")
+getWeather()
+print("Consiguiendo figura...")
+getFigura()
