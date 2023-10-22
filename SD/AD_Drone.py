bf764913c57a2edba78ae7f376cc00a20ea9b46a
slm87-ua <samuellm02@gmail.com>
2023-10-23 00:47:11 +0200

    Dibujo (falta perfeccionarlo)

--- SD/AD_Drone.py
+++ SD/AD_Drone.py
@@ -1,3 +1,5 @@
+
+import json
 import socket
 import sys
 from kafka import KafkaConsumer
@@ -5,28 +7,26 @@ from kafka import KafkaProducer
 import pickle
 import time
 import threading
-import signal
 
 ID=0
 alias = ""
 
 HEADER = 64
-PORT = 5050
+
 FORMAT = 'utf-8'
 FIN = "FIN"
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ca_x = 0
+ca_y = 0
+cd_x = 0
+cd_y = 0
+ad_engine_ip = ""
+ad_engine_port = 0
+broker_ip = ""
+broker_port = 0
+ad_registry_ip = ""
+ad_registry_port = 0
 
-
-#######  REGISTRY   ######
-
-def signal_handler(sig, frame):
-    # Tareas de limpieza aquí, si es necesario
-    print("Cerrando conexión...")
-    client.close()  # Cierra el socket del servidor
-    sys.exit(0)  # Sale del programa
-
-# Asigna el manejador de señales
-signal.signal(signal.SIGINT, signal_handler)
+semaforo = threading.Semaphore(value=1)
 
 def send(msg, client):
     message = msg.encode(FORMAT)
@@ -36,85 +36,192 @@ def send(msg, client):
     client.send(send_length)
     client.send(message)
     
-def start():
-    if  (len(sys.argv) == 4):
-        SERVER = sys.argv[1]
-        PORT = int(sys.argv[2])
-        ADDR = (SERVER, PORT)
-        client.connect(ADDR)
-        print (f"Establecida conexión en [{ADDR}]")
+def registry():
+    global ID, ca_x, ca_y
+    ca_x = 1
+    ca_y = 1
+    alias=input("Introduce tu alias: ")
+    ADDR = (ad_engine_ip, ad_engine_port)  
+        
+    
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(ADDR)
+    print (f"Establecida conexión en [{ADDR}]")
 
-        msg=sys.argv[3]
-        while msg != FIN :
-            print("Realizando solicitud al servidor")
-            send(msg, client)
-            print("Recibo del Servidor: ", client.recv(2048).decode(FORMAT))
-            recibido = client.recv(2048).decode(FORMAT)
-            print(f"{recibido}")
-            alias = msg
-            print(f"{ID}, {alias}")
-            msg=input()
+        #msg=sys.argv[3]
+    print("Realizando solicitud al servidor")
+    send(alias, client)
+    ID = int(client.recv(2048).decode(FORMAT))
+    #print("Recibo del Servidor: ", client.recv(2048).decode(FORMAT))
+    print(f"Recibo del Servidor: {ID}")
+    #ID = client.recv(2048).decode(FORMAT)
 
-        print ("SE ACABO LO QUE SE DABA")
-        print("Envio al servidor: ", FIN)
-        send(FIN)
-        client.close()
-    else:
-        print ("Oops!. Parece que algo falló. Necesito estos argumentos: <ServerIP> <Puerto> <Alias deseado>")
+    client.close()
+    #else:
+    #    print ("Oops!. Parece que algo falló. Necesito estos argumentos: <ServerIP> <Puerto> <Alias deseado>")
 
-start()
+#####        
+
+# RETORNAR A LA CASILLA INICIAL
+def volver():
+    global cd_x, cd_y
+    cd_x = 1
+    cd_y = 1
 
 #######   ENGINE   #######
 
 class Producer(threading.Thread):
+    global ID
     def __init__(self):
         threading.Thread.__init__(self)
+        self.broker_address = f"{broker_ip}:{broker_port}"
         self.stop_event = threading.Event()
+        self.last_sent_message = None  # Almacena el último mensaje enviado a Kafka
 
     def stop(self):
         self.stop_event.set()
 
     def run(self):
-        while True:
-            producer = KafkaProducer(bootstrap_servers="localhost:9092")
-            data = pickle.dumps("Posición a la que me muevo")
-            producer.send('topic_a', data)
+        global semaforo
+        while not self.stop_event.is_set():
+            producer = KafkaProducer(bootstrap_servers=self.broker_address)
 
-            print("Me he movido")
+            position_data = (ID, (ca_x, ca_y))
+            if ca_x == cd_x and ca_y == cd_y:
+                print("He llegado a mi destino")
+                self.stop()
+
+            else:
+                time.sleep(4)
+                print(f"Me he movido a ({ca_x+1}, {ca_y+1})")
+
+            if position_data != self.last_sent_message:
+                semaforo.acquire()
+                serialized_position = pickle.dumps(position_data)
+                producer.send('topic_a', serialized_position)
+
+                # Almacenar el nuevo mensaje como el último mensaje enviado
+                self.last_sent_message = position_data
+
+                # Liberar el permiso del semáforo
+                semaforo.release()
 
             producer.close()
-            time.sleep(4)
+
+def setCoords(x, y):
+    global cd_x, cd_y
+    cd_x = x
+    cd_y = y
+    mov(cd_x, cd_y, ca_x, ca_y)
+
+def mov(cd_x, cd_y, x, y):
+    global ca_x, ca_y 
+    ca_x = x
+    ca_y = y
+    if ca_x == cd_x and ca_y == cd_y:
+        return 1
+    else:
+        distancia_x = cd_x-ca_x
+        distancia_y = cd_y-ca_y
+
+        if distancia_x > 0:
+            ca_x+=1
+        elif distancia_x < 0:
+            ca_x-=1
+
+        if distancia_y > 0:
+            ca_y += 1
+        elif distancia_y < 0:
+            ca_y -= 1
+        return 0
 
 class Consumer(threading.Thread):
+    global ID
     def __init__(self):
         threading.Thread.__init__(self)
+        self.broker_address = f"{broker_ip}:{broker_port}"
         self.stop_event = threading.Event()
 
     def stop(self):
         self.stop_event.set()
 
     def run(self):
-        grupo = '1'
-        consumer2 = KafkaConsumer(bootstrap_servers='localhost:9092',
-                                            auto_offset_reset='latest',
-                                            consumer_timeout_ms=1000, group_id = grupo)
+        try:
+            consumer_coord = KafkaConsumer(bootstrap_servers=self.broker_address ,
+                                                auto_offset_reset='latest',
+                                                consumer_timeout_ms=1000, value_deserializer=lambda x: pickle.loads(x))
+            consumer_mapa = KafkaConsumer(bootstrap_servers=self.broker_address ,
+                                                auto_offset_reset='latest',
+                                                consumer_timeout_ms=1000, value_deserializer=lambda x: pickle.loads(x))
+            
+            consumer_coord.subscribe(['topic_coord'])
+            consumer_mapa.subscribe(['topic_mapa'])
 
-        consumer2.subscribe(['topic_b'])
-
-        while not self.stop_event.is_set():
-            for message in consumer2:
-                print(pickle.loads(message.value))
-
+            while not self.stop_event.is_set():
+                for message in consumer_coord:
+                    datos = message.value
+                    for id, (x, y) in datos.items():
+                        if ID == int(id):
+                            print(f'ID: {id}, Coordenadas:({x}, {y})')
+                            time.sleep(2)
+                            
+                            semaforo.acquire()
+                            setCoords(x-1,y-1)
+                            # Liberar el semáforo después de la actualización del mapa
+                            semaforo.release()
+                            time.sleep(4)
                 if self.stop_event.is_set():
                     break
-                #print("Leyendo mensajes de entrypark")
 
+        except Exception as e:
+            print(f"Error en el consumidor: {e}")
 
-tasks = [start(), Consumer(), Producer()]
+########## MAIN ##########
 
-for t in tasks:
-    t.start()
+def main(argv = sys.argv):
+    print(len(sys.argv))
+    if len(sys.argv) != 7:
+        print("Error: El formato debe ser el siguiente: [IP_Engine] [Puerto_Engine] [IP_Broker] [Puerto_Broker] [IP_Registry] [Puerto_Registry]")
+        sys.exit(1)
+    else:  
+        global ad_engine_ip, ad_engine_port, broker_ip, broker_port, ad_registry_ip, ad_registry_port
+        
+        ad_engine_ip = sys.argv[1]
+        ad_engine_port = int(sys.argv[2])
+        broker_ip = sys.argv[3]
+        broker_port = int(sys.argv[4])
+        ad_registry_ip = sys.argv[5]
+        ad_registry_port = int(sys.argv[6])
+        orden = ""
+        while(orden != "3"):
+            
+            print("¿Qué deseas hacer?")
+            print("1. Registrarse")
+            print("2. Empezar representación")
+            print("3. Apagarse")
+            orden = input()
+            
+            while orden != "1" and orden != "2" and orden != "3":
+                print("Error, indica una de las 3 posibilidades por favor(1, 2 o 3).")
+                orden = input()
 
-while True:
+            if orden == "1":
+                if(ID != 0):
+                    print("Ya estás registrado!")
+                else:
+                    registry()
 
-    time.sleep(1)
+            if orden == "2":
+                tasks = [Consumer(), Producer()]
+
+                for t in tasks:
+                    t.start()
+
+                while True:
+                    time.sleep(1)
+
+            if orden == "3":
+                sys.exit()
+
+if __name__ == "__main__":
+  main(sys.argv[1:])
--- SD/AD_Engine.py
+++ SD/AD_Engine.py
@@ -1,68 +1,41 @@
+#-Conexión con weather
+#-conexión con el fichero de dibujo y obtención num de drones
+#-comprobar num drones en registro.txt
+#-enviar cada pos requerida a cada dron
+#-expresion del mapa a cada mov de dron
+
+import tkinter as tk
 import socket
 import sys
+import threading
 import time
-import signal
-import random
+import os
 from kafka import KafkaProducer
 from kafka import KafkaConsumer
-import time
 import pickle
-import threading
+import signal
 
+
+mapa = {}
 HEADER = 64
-PORT = 5050
 FORMAT = 'utf-8'
-FIN = "FIN"
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ad_engine_port = 0
+MAX_Drones = 0
+broker_ip = ""
+broker_port = 0
+ad_weather_ip = ""
+ad_weather_port = 0
 ciudades = {}
+drones_positions_lock = threading.Lock()
 
-class Consumer1(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-        self.stop_event = threading.Event()
+consumer_thread = None
+producer_thread = None
 
-    def stop(self):
-        self.stop_event.set()
-
-    def run(self):
-        consumer2 = KafkaConsumer(bootstrap_servers='localhost:9092',
-                                            auto_offset_reset='latest',
-                                            consumer_timeout_ms=1000)
-
-        consumer2.subscribe(['topic_a'])
-
-        while not self.stop_event.is_set():
-            for message in consumer2:
-                print(pickle.loads(message.value))
-
-                if self.stop_event.is_set():
-                    break
-                #print("Leyendo mensajes de entrypark")
-
-class Producer(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-        self.stop_event = threading.Event()
-
-    def stop(self):
-        self.stop_event.set()
-
-    def run(self):
-        while True:
-            producer1 = KafkaProducer(bootstrap_servers='localhost:9092')
-            data1 = pickle.dumps("Coord destino")
-            producer1.send('topic_b', data1)
-
-            producer = KafkaProducer(bootstrap_servers='localhost:9092')
-            data = pickle.dumps("Mapa")
-            producer.send('topic_b', data)
-
-            print("Engine enviando mapa")
-            
-            producer.close()
-            time.sleep(3)
+temp =0
+output_lock = threading.Lock()
 
 def obtener_nombre_ciudades():
+    global ciudades
     try:
         # Abrimos el archivo en modo lectura
         with open('ciudades.txt', 'r') as archivo:
@@ -75,23 +48,6 @@ def obtener_nombre_ciudades():
         return "Error: El archivo 'ciudades.txt' no se encuentra.", None
     except Exception as e:
         return f"Error: {e}", None
-
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    client.send(send_length)
-    client.send(message)
-
-def signal_handler(sig, frame):
-    # Tareas de limpieza aquí, si es necesario
-    print("Cerrando conexión...")
-    client.close()  # Cierra el socket del servidor
-    sys.exit(0)  # Sale del programa
-
-# Asigna el manejador de señales
-signal.signal(signal.SIGINT, signal_handler)
 
 def procesar_archivo_registro():
     try:
@@ -110,67 +66,256 @@ def procesar_archivo_registro():
     except Exception as e:
         return f"Error: {e}", None
 
+def signal_handler(sig, frame):
+    # Tareas de limpieza aquí, si es necesario
+    if consumer_thread:
+        consumer_thread.stop()
+    if producer_thread:
+        producer_thread.stop()
+    sys.exit(0)  # Sale del programa
+
+# Asigna el manejador de señales
+signal.signal(signal.SIGINT, signal_handler)
+
+def send(msg, client):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
     
-########## MAIN ##########8
+#HAY QUE CORREGIRLO
+def getWeather(ciudad, SERVER, PORT):
+    while True:
+        msg = ciudad
+        with output_lock:
+            if  (len(sys.argv) == 3):
+                ADDR = (Ad, PORT)
+                
+                client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                client.connect(ADDR)
+
+                obtener_nombre_ciudades()
+                contador = 0
+                ciudad_random = random.choice(list(ciudades.keys()))
+                
+                send(ciudad_random, client)
+                print("Recibo del Servidor: ", client.recv(2048).decode(FORMAT))
+                client.close()
+
+        time.sleep(10)
+
+def getFigura():
+    datos = {}
+    with open("Figura.txt", 'r') as file:
+        for linea in file:
+            partes = linea.strip().split()
+            if len(partes) == 3:
+                ID = partes[0]
+                x = int(partes[1])
+                y = int(partes[2])
+                datos[ID] = (x, y)
+    return datos
 
 
-def start():
+#########  DRONES MAPA  ###########
+root = tk.Tk()
+root.title("ART WITH DRONES")
+
+titulo = tk.Label(root, text="ART WITH DRONES", font=("Arial", 16, "bold"), pady=10, bg="black", fg="white")
+titulo.pack()
+# Crear un lienzo para dibujar el mapa
+canvas = tk.Canvas(root, width=600, height=600, bg="white")
+canvas.pack()
+
+
+
+# Función para dibujar el mapa con las coordenadas
+def dibujar_mapa(mapa):
+    canvas.delete("casilla")  # Limpiar solo las casillas antes de redibujar el mapa
+    tamaño_casilla = 30  # Tamaño de cada casilla en píxeles
     
-    if  (len(sys.argv) == 4):
-        SERVER = sys.argv[1]
-        PORT = int(sys.argv[2])
-        ADDR = (SERVER, PORT)
+    for x in range(20):
+        for y in range(20):
+
+            x1 = x * tamaño_casilla
+            y1 = y * tamaño_casilla
+            x2 = x1 + tamaño_casilla
+            y2 = y1 + tamaño_casilla
+            # Dibujar la casilla delimitada por una línea negra
+            canvas.create_rectangle(x1, y1, x2, y2, fill="white", outline="black", tags="casilla")
+          
+  
+    for x in range(20):
+        for y in range(20):
+            x1 = x * tamaño_casilla
+            y1 = y * tamaño_casilla
+            x2 = x1 + tamaño_casilla
+            y2 = y1 + tamaño_casilla
+            
+            ids_drones = [id_dron for id_dron, coordenadas in mapa.items() if coordenadas == (x, y)]
+            if ids_drones:
+                # Convertir los IDs de drones a una cadena separada por comas
+                ids_str = ", ".join(map(str, ids_drones))
+                # Dibujar el ID del dron en la casilla
+                centro_x = (x1 + x2) // 2
+                centro_y = (y1 + y2) // 2
+                texto = canvas.create_text(centro_x, centro_y, text=ids_str, fill="red", font=("Arial", 10, "bold"), tags="casilla")
+                # Obtener las coordenadas del texto en el lienzo
+                x, y, _, _ = canvas.bbox(texto)
+    
+                # Ajustar el tamaño de fuente si el texto se sale de la casilla
+                while x1 + 5 > x or x2 - 5 < x or y1 + 5 > y or y2 - 5 < y:
+                    tamaño_fuente = int(canvas.itemcget(texto, "font").split()[1])
+                    tamaño_fuente -= 1
+                    canvas.itemconfig(texto, font=("Arial", tamaño_fuente, "bold"))
+                    x, y, _, _ = canvas.bbox(texto)
+            else:
+                pass
+          
+dibujar_mapa({})
+
+class Consumer(threading.Thread):
+    global mapa
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.broker_address = f"{broker_ip}:{broker_port}"
+        self.stop_event = threading.Event()
+        #self.mapa_lock = threading.Lock()  # Crear un objeto de bloqueo
+
+    def stop(self):
+        self.stop_event.set()
+
+    def actualizar_mapa(self, position_data):
         try:
-            tiempo_de_consulta = int(sys.argv[3])
-        except ValueError:
-            print("El tiempo de consulta debe ser un número entero.")
-            sys.exit()
+            id, (x, y) = position_data
+        except (TypeError, ValueError):
+            print("Datos de posición inválidos recibidos del dron.")
+            return # Adquirir el bloqueo antes de realizar actualizaciones en el mapa
         
-        client.connect(ADDR)
-        print (f"Establecida conexión en [{ADDR}]")
-        obtener_nombre_ciudades()
-        contador = 0
-        ciudad_random = random.choice(list(ciudades.keys()))
-        msg = ciudad_random
-        while contador < 10:
-            print("Envio al servidor: ", msg)
-            send(msg)
-            print("Recibo del Servidor: ", client.recv(2048).decode(FORMAT))
-            # Pausar el programa durante el tiempo de consulta antes de enviar la próxima consulta
-            time.sleep(tiempo_de_consulta)
-            contador += 1
+        # Adquirir el Lock antes de modificar drones_positions
+       # drones_positions_lock.acquire()
 
-        client.close()
+        try:
+            mapa[id] = (x, y)
+            print(f"X: {x}, Y: {y}")
+        finally:
+            pass
+            # Liberar el Lock sin importar si ocurre una excepción
+           # drones_positions_lock.release()
 
-    else:
-        print ("Oops!. Parece que algo falló. Necesito estos argumentos: <ServerIP> <Puerto> <Ciudad> <Tiempo consulta>")
+
+    def run(self):
+        try:
+            consumer = KafkaConsumer(bootstrap_servers= self.broker_address,
+                                                auto_offset_reset='latest',
+                                                consumer_timeout_ms=1000)
+
+            consumer.subscribe(['topic_a'])
+
+            while not self.stop_event.is_set():
+                for message in consumer:
+                    try:
+                        position_data = pickle.loads(message.value)
+                        if isinstance(position_data, tuple) and len(position_data) == 2:
+                            self.actualizar_mapa(position_data)
+                            print(position_data)
+                            print(mapa)
+                            root.after(10, dibujar_mapa, mapa)
+                        else:
+                            print("Datos inválidos recibidos del dron.")
+                    except Exception as e:
+                        print(f"Error al procesar el mensaje: {e}")
+
+                    if self.stop_event.is_set():
+                        break
+               
+        except Exception as e:
+            print(f"Error en el consumidor: {e}")
+        
+
+class Producer(threading.Thread):
+    global mapa
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.broker_address = f"{broker_ip}:{broker_port}"
+        self.stop_event = threading.Event()
+        self.mapa_lock = threading.Lock()  # Objeto de bloqueo para proteger el mapa
+
+
+    def stop(self):
+        self.stop_event.set()
+
+    def run(self):
+        while not self.stop_event.is_set():
+            datos = getFigura()
+            
+            producer_coor = KafkaProducer(bootstrap_servers=self.broker_address )
+            coordenadas = pickle.dumps(datos)
+            producer_coor.send('topic_coord', coordenadas)
+            
+            time.sleep(3)
+            producer_mapa = KafkaProducer(bootstrap_servers=self.broker_address)
+            mapa_serializado = pickle.dumps(mapa)
+            producer_mapa.send('topic_mapa', mapa_serializado)
+            
+            producer_coor.close()
+            producer_mapa.close()
+            pass
+
+########## MAIN ##########
 
 def main(argv = sys.argv):
-    """"
-    PORT = argv[1]
-    MAX_Drones = argv[2]
-    Server_Kafka = argv[3]
-    Port_Kafka = argv[4]
-    Server_W = argv[5]
-    Port_W = argv[6]
-    """
-
-    print("Obteniendo Clima")
-  
-    
-    print("Conecatnado con Dron")
+    global ad_engine_port, MAX_Drones, broker_ip, broker_port, ad_weather_ip, ad_weather_port, mapa
+    ad_engine_port = argv[0]
+    MAX_Drones = argv[1]
+    broker_ip = argv[2]
+    broker_port = argv[3]
 
     tam = len(argv)
 
+    datos = getFigura()
+    print(datos)
+
     try:
         if tam == 7:
+            
+            ad_engine_port = argv[0]
+            MAX_Drones = argv[1]
+            broker_ip = argv[2]
+            broker_port = argv[3]
+            ad_weather_ip = argv[4]
+            ad_weather_port = argv[5]
+            ###  WEATHER  ###
             ADDR = (Server_W, Port_W)
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.connect(ADDR)
+            print (f"Establecida conexión en [{ADDR}]")
 
-        tasks = [getWeather(), Consumer1(), Producer()]
+            msg=sys.argv[3]
+            contador = 0
+            while contador < 10:
+                print("Envio al servidor: ", msg)
+                send(msg)
+                print("Recibo del Servidor: ", client.recv(2048).decode(FORMAT))
+                
+                # Pausar el programa durante el tiempo de consulta antes de enviar la próxima consulta
+                time.sleep(10)
+                contador += 1
+            client.close()
 
+        ###  KAFKA  ###
+
+        tasks = [Consumer(), Producer()]
+        # Acceder a la instancia del consumidor desde la lista de tareas
+        consumer_thread = tasks[0]
+        producer_thread = tasks[1]
         for t in tasks:
             t.start()
 
+        # Iniciar el bucle principal de la interfaz gráfica
+        root.mainloop()
         while True:
 
             time.sleep(1)
@@ -178,25 +323,12 @@ def main(argv = sys.argv):
     except:
         pass
 
-    msg=input("¿En que ciudad se va a actuar? ")
-    ciudad = msg
-    print("Consiguiendo información del tiempo...")
-    #getWeather()
-    thread =threading.Thread(target = getWeather, args = (ciudad, ))
-    thread.daemon = True
-    thread.start()
-    while True:
+# Crear un mapa de 20x20 inicializado con ceros (posición vacía)
 
-        print("Consiguiendo figura...")
-        datos = {}
-        #getFigura("Figura.txt")
-        time.sleep(60)
-        #connectDron()
-        
-start()
-# Crear una matriz 2D de 20x20 posiciones para representar el espacio aéreo
-# espacio_aereo = [[0 for _ in range(20)] for _ in range(20)]
 
-# Imprimir la matriz para visualizar el espacio aéreo
-# for fila in espacio_aereo:
-    # print(fila)
+if __name__ == "__main__":
+    main(sys.argv[1:])
+    pass
+
+
+    #  python3 AD_Engine.py 5050 20 127.0.0.1 5050 127.0.0.1 5050
