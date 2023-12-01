368d9b4d516d44c86f4b947d6d4afd2fd687cda2
abt39-ua <71825176+abt39-ua@users.noreply.github.com>
2023-12-01 10:10:14 +0100

    Update AD_Registry.py

--- SD/AD_Registry.py
+++ SD/AD_Registry.py
@@ -1,10 +1,10 @@
-import socket, ssl
+import socket 
 import threading
 import os
 import signal
 import sys
+from pymongo import MongoClient, server_api
 
-nom_archivo = "registro.txt"
 ID = 1
 HEADER = 64
 ADDR = 0
@@ -13,19 +13,45 @@ ad_registry_ip = 0
 FORMAT = 'utf-8'
 FIN = "FIN"
 MAX_CONEXIONES = 100
+opcion_borrar = -1
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-semanforo = threading.Semaphore(value=1)
+
+# Conexión a MongoDB con una versión específica de la API del servidor
+uri = "mongodb://localhost:27018"
+api_version = server_api.ServerApi('1', strict=True, deprecation_errors=True)
+client = MongoClient(uri, server_api=api_version)
+dbName = 'SD'
+colletionName = 'drones'
+
+# Conexión a MongoDB
+client = MongoClient("mongodb://localhost:27017/")
+# Conectar al servidor de MongoDB
+client.admin.command('ismaster')
+
+# Obtener la base de datos
+db = client['SD']
+
+# Obtener la colección
+collection = db['drones']
+
+def conectar_db():
+    # Conectar a la instancia de MongoDB
+    client = MongoClient('localhost', 27018)  # Asegúrate de cambiar el puerto si es diferente
+    # Seleccionar la base de datos "SD"
+    db = client['SD']
+    # Seleccionar la colección "drones"
+    collection = db['drones']
+    return collection
 
 def buscar_alias(alias):
-    with open("registro.txt", 'r') as file:
-        for line in file:
-            if f"Alias: {alias}" in line:
-                parts = line.split(", ")
-                for part in parts:
-                    if part.startswith("ID: "):
-                        return int(part.split(":")[1].strip())
-                break
-    return "0"
+    collection = conectar_db()
+    # Buscar si el alias ya existe en la base de datos
+    document = collection.find_one({"nombre": alias})
+    print(document)
+    if document:
+        return document["_id"]
+    else:
+        return "0"
 
 def signal_handler(sig, frame):
     global server
@@ -37,49 +63,6 @@ def signal_handler(sig, frame):
 # Asigna el manejador de señales
 signal.signal(signal.SIGINT, signal_handler)
 
-#############   API_REST   ###########
-cert = 'certServ.pem'
-
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-context.load_cert_chain(cert, cert)
-
-bindsocket = socket.socket()
-bindsocket.bind((bytearray(ad_registry_ip), ad_registry_port))
-bindsocket.listen(5)
-
-def deal_with_client(connstream):
-    msg_length = connstream.recv(1024)#.decode(FORMAT)
-    #data = connstream.recv(int(msg_length)).decode(FORMAT)
-    print('Recibido', repr(msg_length))
-    print("Enviando info")
-    connstream.send(b'Info uwu')
-
-def connect_API():
-    print('Escuchando en:', ad_registry_ip, ad_registry_port)
-
-    while True:
-        newsocket, fromaddr = bindsocket.accept()
-        connstream = context.wrap_socket(newsocket, server_side=True)
-        print('Conexion recibida')
-        try:
-            deal_with_client(connstream)
-        finally:
-            connstream.shutdown(socket.SHUT_RDWR)
-            connstream.close()
-
-""""
-    while True:
-        newsocket, fromaddr = bindsocket.accept()
-        connstream = context.wrap_socket(newsocket, server_side=True)
-        print('Conexion recibida')
-        try:
-            deal_with_client(connstream)
-        finally:
-            connstream.shutdown(socket.SHUT_RDWR)
-            connstream.close() 
-            """
-
-#############   SOCKETS   ############
 
 def handle_client(conn, addr):
     global ID
@@ -87,32 +70,23 @@ def handle_client(conn, addr):
     connected = True
     while connected:
         msg_length = conn.recv(HEADER).decode(FORMAT)
-        print("A", msg_length, "Z")
         if msg_length:
-            #msg_length = int(msg_length)
-            print(msg_length)
-            #msg = msg_length#conn.recv(msg_length).decode(FORMAT)
-            msg = conn.recv(HEADER).decode(FORMAT)
-            print(msg)
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
             n = buscar_alias(msg)
             print(f"He recibido del cliente [{addr}] el mensaje: {msg}")
-            print(ID)
+            print()
+            print(n)
             if n == "0":
                 conn.send(f"{ID}".encode(FORMAT))
                 save_info(ID, msg)
-                ID = ID + 1
+                ID += 1
 
             else:
                 conn.send(f"Este Dron ya estaba registrado con el ID: {n}".encode(FORMAT))
 
     conn.close()
 
-def is_utf8(data):
-    try:
-        data.decode('utf-8')
-        return True
-    except UnicodeDecodeError:
-        return False
 
 def start():
     global server, ad_registry_ip
@@ -122,45 +96,75 @@ def start():
         print(f"[LISTENING] Servidor a la escucha en {ad_registry_ip}")
         print()
         CONEX_ACTIVAS = threading.active_count()-1
-
         while True:
             conn, addr = server.accept()
             CONEX_ACTIVAS = threading.active_count()
-
-            semanforo.acquire()
-            try:
-                if is_utf8(conn.recv(HEADER)) == False:
-                    thread = threading.Thread(target=connect_API)
-                    thread.start()
-                    print("a")
-                else:
-                    thread = threading.Thread(target=handle_client, args=(conn, addr))
-                    thread.start()
-                    print("b")
-            finally:
-                semanforo.release()
-
-            print(f"[CONEXIONES ACTIVAS] {CONEX_ACTIVAS}")
-            print("CONEXIONES RESTANTES PARA CERRAR EL SERVICIO:", MAX_CONEXIONES-CONEX_ACTIVAS)
-            conn.close()
-            CONEX_ACTUALES = threading.active_count()-1
+            if (CONEX_ACTIVAS <= MAX_CONEXIONES): 
+                thread = threading.Thread(target=handle_client, args=(conn, addr))
+                thread.start()
+                print(f"[CONEXIONES ACTIVAS] {CONEX_ACTIVAS}")
+                print("CONEXIONES RESTANTES PARA CERRAR EL SERVICIO:", MAX_CONEXIONES-CONEX_ACTIVAS)
+            else:
+                conn.close()
+                CONEX_ACTUALES = threading.active_count()-1
     except Exception as e:
         print(f"Error al iniciar el servidor: {str(e)}")
 
 def save_info(ID, alias):
-    # Se comprueba que el archivo está creado y si no lo esta, lo crea
     try:
-        with open(nom_archivo, 'a') as registro:
-            registro.write(f"ID: {ID}, Alias: {alias}\n")
-        print("Información guardada con éxito.")
-        print()
+        collection = conectar_db()
+        print("Conexión exitosa a la base de datos")
+
+        existing_document = collection.find_one({"nombre": alias})
+        if existing_document:
+            print(f"Error al insertar la información en la base de datos: El alias '{alias}' ya existe.")
+        else:
+            # Se realiza la inserción en la colección "drones" con el ID especificado
+            collection.insert_one({"_id": ID, "nombre": alias})
+            print("Información insertada con éxito en la base de datos.")
+            print()
+            consultar_info()
+
+    except errors.DuplicateKeyError as e:
+        print(f"Error al insertar la información en la base de datos: {_id} ya existe.")
+    
     except Exception as e:
-        print(f"Error al guardar la información: {str(e)}")
+        print(f"Error al insertar la información en la base de datos: {str(e)}")
+
+def consultar_info():
+    try:
+        collection = conectar_db()
+        print("Conexión exitosa a la base de datos")
+
+        # Consultar todos los documentos en la colección "drones"
+        cursor = collection.find()
+
+        # Imprimir los documentos
+        for document in cursor:
+            print(document)
+
+    except Exception as e:
+        print(f"Error al consultar la información en la base de datos: {str(e)}")
+
+
+def borrar_registros_db():
+    try:
+        collection = conectar_db()
+        print("Conexión exitosa a la base de datos")
+
+        # Elimina todos los registros de la colección "drones"
+        result = collection.delete_many({})
+
+        print(f"{result.deleted_count} registros en la base de datos eliminados.")
+        consultar_info()
+    except Exception as e:
+        print(f"Error al borrar registros de la base de datos: {str(e)}")
+
 
 ######################### MAIN ##########################
 
 def main(argv = sys.argv):
-    global ad_registry_port, ad_registry_ip, server, ADDR
+    global ad_registry_port, ad_registry_ip, server, ADDR, opcion_borrar, ID
     if len(sys.argv) != 4:
         print("Error: El formato debe ser el siguiente: [Puerto_escucha] [IP_Registry] [Opcion_Borrar]")
         sys.exit(1)
@@ -173,9 +177,15 @@ def main(argv = sys.argv):
         # Verificar la opción de borrar archivo
         opcion_borrar = int(sys.argv[3])
         if opcion_borrar == 0:
-            with open(nom_archivo, 'w') as registro:
-                pass
+            borrar_registros_db()
+            print("Registros en la base de datos eliminados.")
         elif opcion_borrar == 1:
+            collection = conectar_db()
+            last_document = collection.find_one({}, sort=[("_id", -1)])  # Obtener el último documento insertado
+            if last_document:
+                last_id = last_document["_id"]
+                ID = last_id + 1  # Establecer ID al siguiente ID disponible
+
             # Conservar el registro anterior (no borrar archivo)
             pass
         else: 
@@ -183,13 +193,7 @@ def main(argv = sys.argv):
             sys.exit(1)
         print("[STARTING] Servidor inicializándose...")
 
-        api_thread = threading.Thread(target=connect_API)
-        api_thread.start()
-
-        socket_thread = threading.Thread(target=start)
-        socket_thread.start()
+        start()
 
 if __name__ == "__main__":
   main(sys.argv[1:])
-
-# python3 AD_Registry.py 5051 127.0.0.1 0 
